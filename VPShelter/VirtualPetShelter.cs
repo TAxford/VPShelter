3042f0a8833432a86f2a857b1be680b1377a59ab
TAxford <t.axford2@yahoo.com>
2018-02-09 10:41:20 -0500

    Added VirtualPetShelter class

---
+++ VPShelter/VirtualPetShelter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class VirtualPetShelter
+    {
+        //fields
+        private int totalPetAmmount;
+        private int totalPetFeed;
+        private int totalVolunteers;
+        private int totalEmployees;
+        private int totalManagers;
+
+        //Properties
+        public int TotalPetAmount { get; set; }
+        public int TotalPetFeed { get; set; }
+        public int TotalVolunteers { get; set; }
+        public int TotalEmployees { get; set; }
+        private int TotalManagers { get; set; }
+
+    }
+}
