ddb74dedba04c1d4eb7e59b322b0f3b9cc5a31fc
TAxford <t.axford2@yahoo.com>
2018-02-09 10:30:47 -0500

    Added Hippo class

---
+++ VPShelter/Hippo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class Hippo : VirtualPet
+    {
+        public Hippo()
+        {
+            //default constructor
+        }
+
+        public Hippo(string name, int hunger, int thirst, int boredom)
+        {
+            name = name;
+            hunger = hunger;
+            thirst = thirst;
+            boredom = boredom;
+        }
+
+    }
+}
